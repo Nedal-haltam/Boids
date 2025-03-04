265e77bc9c0bf9ae2d2dccf29594dfcc633dfbfc
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-03-04 14:39:10 +0300

    create project and add raylib

---
+++ Boids/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Raylib_cs;
+namespace Boids
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.SetConfigFlags(ConfigFlags.AlwaysRunWindow | ConfigFlags.ResizableWindow);
+            Raylib.InitWindow(800, 600, "hello");
+            while(!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Raylib_cs.Color.Red);
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
